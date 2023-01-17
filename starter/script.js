ab08a148b0d6d70d197ecb6e27b7a2a801d16868
laurabbity915 <laura_yuen915@outlook.com>
2023-01-17 13:01:36 +0000

    "Your secure password" display

--- starter/script.js
+++ starter/script.js
@@ -90,33 +90,53 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  console.log( document.getElementById("upper").checked);
-  console.log( document.getElementById("lower").checked);
-  console.log( document.getElementById("number").checked);
-  console.log( document.getElementById("specialcharacters").checked);
-  console.log( document.getElementById("quantity").value);
+  console.log(document.getElementById("upper").checked);
+  console.log(document.getElementById("lower").checked);
+  console.log(document.getElementById("number").checked);
+  console.log(document.getElementById("specialcharacters").checked);
+  console.log(document.getElementById("quantity").value);
+
+  let passwordCharSet = [];
+
+  if (document.getElementById("upper").checked == true) {
+    passwordCharSet.push(...upperCasedCharacters);
+  }
+  if (document.getElementById("lower").checked == true) {
+    passwordCharSet.push(...lowerCasedCharacters);
+  }
+  if (document.getElementById("number").checked == true) {
+    passwordCharSet.push(...numericCharacters);
+  }
+  if (document.getElementById("specialcharacters").checked == true) {
+    passwordCharSet.push(...specialCharacters);
+  }
+
+  return passwordCharSet;
+
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  console.log("on click generatePassword");
+
+  let mergedCharSet = getPasswordOptions();
+  let generatedPassword = '';
+  for(let i = 0; i < document.getElementById("quantity").value; i++){
+    generatedPassword += getRandom(mergedCharSet);
+  }
+  writePassword(generatedPassword);
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(password) {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-//generateBtn.addEventListener('click', writePassword);
